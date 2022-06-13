e9e43377e2b57b5b95d9c054fc153eed7bcd5c28
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-13 12:49:42 +0600

    Remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import 'react-loading-skeleton/dist/skeleton.css';
-import { LoaderContext } from './contexts/LoaderContext';
 import GlobalContextProvider from './contexts/GlobalContextProvider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
