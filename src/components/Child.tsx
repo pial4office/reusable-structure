afff958732c22af0dc0c8afa058feb3604cc212e
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-13 16:15:12 +0600

    Fix typescript error issue

--- src/components/Child.tsx
+++ src/components/Child.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 import getToDO from '../test/getToDo';
 import { TypeToDO } from '../type/toDo';
+import { AxiosError } from 'axios';
 
 export default function Child() {
   const [toDo, setToDo] = useState<TypeToDO>();
 
   async function getMyData() {
     const response = await getToDO<TypeToDO>();
-    // console.table(response);
-    if (response.name !== 'AxiosError') {
+    console.log(typeof response);
+    if ((response as AxiosError).name !== 'AxiosError') {
       console.log(response);
-      setToDo(response);
+      setToDo(response as TypeToDO);
       console.log('error not occured');
     } else {
       console.log(response);
