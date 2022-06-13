c1c04b951f8dc7390f989667c6f3f0e82cd3d946
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-13 16:33:43 +0600

    Solve type error

--- src/api/method.ts
+++ src/api/method.ts
@@ -1,5 +1,5 @@
 import axiosInstance from './axiosInstance';
 
-export function getData<Type>(url: string): Type {
+export function getData<Type>(url: string) {
   return axiosInstance.get<Type>(url);
 }
