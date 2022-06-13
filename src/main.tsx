d4ddecc359cf69b4d669e5df98b1a586339608cb
Md. Pial Ahamed <pial4office@gmail.com>
2022-06-13 18:22:46 +0600

    Add toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <>
+    <Toaster position="top-center" reverseOrder={false} />
     <App />
-  </React.StrictMode>
-)
+  </>
+);
